98b543868150819855d67145ca4961355868dfc2
SFGibby <samgibson124@gmail.com>
2020-09-16 08:18:46 -0600

    yesterdays assignment, classes made a lot of sense, but man closures are weird

--- classes.js
+++ classes.js
@@ -29,8 +29,19 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
 
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' Widget'
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -47,7 +58,21 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
+
+
+  hire(employee){
+    this.reports.push(employee);
+  }
+
+  fire(index){
+    this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -71,7 +96,44 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+    super(first_name, last_name, email, age, reports);
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+  hire(employee){
+    super.hire(employee);
+    if(this.reports.length === 0){
+      return this.title = "Not a manager"
+    }
+    else if (this.reports.length >= 1 && this.reports.length <=3){
+      return this.title = "Barely Manager";
+
+    }
+    else if (this.reports.length >= 4 && this.reports.length <=10){
+      return this.title = "Mostly Manager";
+
+    }
+    else if (this.reports.length >= 11 && this.reports.length <=50){
+      return this.title = "Manager";
+
+    }
+    else if (this.reports.length >= 51 && this.reports.length <=100){
+      return this.title = "Manager Plus";
+    }
+    else {
+      return this.title = "Bestest Manager";
+    }
+  }
+
+  fire(index){
+    super.fire()
+      return this.bonus +=100;
+    }
+  
+  
+}
 
 
 
@@ -98,6 +160,9 @@
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+// makeWidgets(num) {
+//   this.widgets_made_count + (num);
+//   this.wear_and_tear_count = this.widgets_made_count /50;
+// }
 
 
--- closures.js
+++ closures.js
@@ -21,14 +21,10 @@ function outer() {
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
-// Code Here
-
-
 
 //Once you do that, invoke inner.
-
-//Code Here
+const inner = outer();
+inner();
 
 
 
@@ -51,7 +47,9 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+const callJake = callFriend('Jake');
+
+
 
 
 
@@ -61,16 +59,32 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter() {
+  let count = 0;
 
+  return function() {
+    count += 1;
+    return count;
+  }
+}
 
+// function makeCounter() {
+//   let count = 0;
+
+//   function increaseThatNum() {
+//     count = count + 1;
+//     return count;
+//   }
+
+//   return increaseThatNum;
+// }
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -86,18 +100,25 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
+  let count = value
 
   return {
-
+    inc: function() {
+      count += 1;
+      return count;
+    },
+    dec: function() {
+      count -= 1;
+      return count;
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -112,10 +133,12 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+  function message() {
+    return `${welcomeText} ${firstname} ${lastname}.`
+  }
 
-  //Uncommment this to return the value of your message function
-  //return message;
+
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,9 +166,13 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod: function() {
+      return privateMethod();
+    }
   };
 })();
+privateMethod();
+module.publicMethod();
 
 
 
@@ -162,7 +189,14 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: function(num) {
+      secret += num;
+      return secret;
+    },
+    takeAwayFromSecret: function(num) {
+      secret -= num;
+      return secret;
+    }
   };
 }
 
@@ -173,7 +207,7 @@ function secretNumber() {
 /*
   Here we have a for loop that will iterate as long as i is less than or equal to 5.
   What we need to do is console.log(i) so that it logs like so:
-    0 second after call - log 0
+    0 seconds after call - log 0
     1 seconds after call - log 1
     2 seconds after call - log 2
     3 seconds after call - log 3
@@ -186,11 +220,28 @@ function secretNumber() {
   Fix the code below to log the desired output.
 */
 
+// function timeOutCounter() {
+//   for (let i = 0; i <= 5; i++) {
+//     setTimeout(function() {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+// }
+// timeOutCounter();
+
+
 function timeOutCounter() {
+
   for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+    function theValue(num){
+        setTimeout(function() {
+      console.log(num);
+      }, num * 1000);
+    }
+    theValue(i);
+
+
+
   }
 }
 timeOutCounter();
